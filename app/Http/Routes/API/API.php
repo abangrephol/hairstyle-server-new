c4492a050bef194c73e9c8405d2c58ec45f0ddba
abangrephol <abang.zeze@gmail.com>
2016-02-17 16:30:35 +0700

    API route fix

--- app/Http/Routes/API/API.php
+++ app/Http/Routes/API/API.php
@@ -2,9 +2,9 @@
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
     $api->group(['namespace' => '\App\Http\Controllers\API'], function($api) {
-        $api->get('check/{api}', 'APIController@check');
+        $api->get('check/{imei}/{api}', 'APIController@check');
         $api->get('hairstyles/{api}', 'APIController@hairstyles');
         $api->get('frames/{api}', 'APIController@frames');
-        $api->get('categories/{imei}/{api}', 'APIController@categories');
+        $api->get('categories/{api}', 'APIController@categories');
     });
 });
